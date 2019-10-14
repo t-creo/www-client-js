b927deb696b41fbb3c0d1a24fb603e82441b247c
German Robayo <germanrobayo33@gmail.com>
2019-10-13 23:27:48 -0400

    add get twitter usercred by userId endpoint

--- src/index.ts
+++ src/index.ts
@@ -54,12 +54,18 @@ export default class WorldWhiteWebClient {
     return response.data
   }
 
-  async getPlainTextCredibility(weights: TextCredibilityWeights, text: PlainText) {
+  async getPlainTextCredibility(
+    weights: TextCredibilityWeights, text: PlainText) : Promise<Credibility> {
     const response = await this.client.get('/calculate/plain-text', {
       params: {
         ...weights, ...text
       }
     })
+    return response.data
+  }
+
+  async getTwitterUserCredibility(userId: string) : Promise<Credibility> {
+    const response = await this.client.get(`/calculate/twitter/user/${userId}`)
     return response.data
   }
 }

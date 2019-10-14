58be38b247d8f391055e3112b21450d9caf168da
German Robayo <germanrobayo33@gmail.com>
2019-10-13 23:12:44 -0400

    add skeleton of www client

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,60 @@
-export const Greeter = (name: string) => `Hello ${name}`
+import axios, { AxiosInstance } from 'axios'
+
+export interface Health {
+    status: 'UP'
+}
+
+export interface Credibility {
+  credibility: number
+}
+
+export interface TextCredibilityWeights {
+  weightSpam: number
+  weightBadWords: number
+  weightMisspelling: number
+}
+
+export interface TweetCredibilityWeights extends TextCredibilityWeights {
+  weightText: number
+  weightSocial: number
+  weightUser: number
+}
+
+export interface TwitterUser {
+  name: string
+  verified: boolean
+  yearJoined: number
+  followersCount: number
+  friendsCount: number
+}
+
+export interface Tweet {
+  text: Text
+  user: TwitterUser
+}
+
+export interface Text {
+  text: string
+  lang: Language
+}
+export type Language = 'es' | 'en' | 'fr'
+
+export default class WorldWhiteWebClient {
+    client: AxiosInstance
+
+    constructor(baseUrl: string = "http://world-white-web.herokuapp.com") {
+        this.client = axios.create({
+            baseURL: baseUrl
+        })
+    }
+
+    async getHealth() : Promise<Health> {
+        try {
+            const response = await this.client.get('/health')
+            return response.data
+        } catch (e) {
+            throw e
+        }
+    }
+
+}
